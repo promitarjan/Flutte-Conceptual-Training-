9800e27290b6cbe1ff9862a49b594894a68d20f4
promitarjan <87933788+promitarjan@users.noreply.github.com>
2026-08-07 03:27:11 +0600

    step 2 done

--- Container/my_first_container/lib/main.dart
+++ Container/my_first_container/lib/main.dart
@@ -6,13 +6,37 @@ void main() {
 
 class MyFirstContainer extends StatelessWidget {
   const MyFirstContainer({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            height: 100,
+            width: 150,
+            child: const Text("Step 2 Complete"),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+/*
+Step 1: 
+class MyFirstContainer extends StatelessWidget {
+  const MyFirstContainer({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(child: Container(child: Text("Step 1 Complete"))),
+        body: Center(child: Container(child: const Text("Step 1 Complete"))),
       ),
     );
   }
 }
+*/
