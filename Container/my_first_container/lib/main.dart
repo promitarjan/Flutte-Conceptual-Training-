709bd2066396a6e22b68df373997c0106bd87206
promitarjan <87933788+promitarjan@users.noreply.github.com>
2026-08-07 03:47:08 +0600

    learned padding inside container

--- Container/my_first_container/lib/main.dart
+++ Container/my_first_container/lib/main.dart
@@ -13,6 +13,31 @@ class MyFirstContainer extends StatelessWidget {
       home: Scaffold(
         body: Center(
           child: Container(
+            height: 100,
+            width: 200,
+            color: Colors.blue,
+            padding: EdgeInsets.only(left: 30.0, top: 20.0),
+            child: const Text(
+              "Step 4 Complete",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/*
+class MyFirstContainer extends StatelessWidget {
+  const MyFirstContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Container(
             width: 200,
             height: 100,
             color: Colors.blue,
@@ -26,6 +51,7 @@ class MyFirstContainer extends StatelessWidget {
     );
   }
 }
+*/
 
 /*
 Step 2:
